89a12e7817417cc2919cd6643d49e11028e5466b
valenadels <vadelsflugel@fi.uba.ar>
2025-11-03 23:30:59 -0300

    initial table

--- dtnsim/src/node/dtn/routing/RoutingAntop.cc
+++ dtnsim/src/node/dtn/routing/RoutingAntop.cc
@@ -3,9 +3,12 @@
 
 #include "src/node/mobility/SatSGP4Mobility.h"
 
+const double TTL_SEC = 6;
+
 RoutingAntop::RoutingAntop(Antop* antop, int eid, SdrModel *sdr, map<int, SatSGP4Mobility*> *mobilityMap): RoutingDeterministic(eid, sdr, nullptr) { //TODO check this null
     this->prevSrc = 0;
     this->antopAlgorithm = antop;
+    this->nextHopCache = unordered_map<int, CacheEntry>();
     this->mobilityMap = mobilityMap;
 }
 
@@ -14,6 +17,17 @@ RoutingAntop::~RoutingAntop() {}
 void RoutingAntop::routeAndQueueBundle(BundlePkt *bundle, double simTime) {
     std::cout << "Node " << eid_ << " routing bundle " << bundle->getBundleId() << " from src " << bundle->getSourceEid() << ", sender " << bundle->getSenderEid() << " to " << bundle->getDestinationEid() << std::endl;
 
+    int cachedNextHop = getFromCache(bundle->getDestinationEid(), simTime);
+    if(cachedNextHop != 0){
+        bundle->setNextHopEid(cachedNextHop);
+        std::cout << "Using cached next hop " << cachedNextHop << " for bundle " << bundle->getBundleId() << std::endl;
+        return;
+    }
+
+    getNewNextHop(bundle, simTime);
+}
+
+void RoutingAntop::getNewNextHop(BundlePkt *bundle, double simTime){
     const vector<H3Index> candidates = this->antopAlgorithm->getHopCandidates(
         getCurH3IndexForEid(eid_),
         getCurH3IndexForEid(bundle->getDestinationEid()),
@@ -30,7 +44,7 @@ void RoutingAntop::routeAndQueueBundle(BundlePkt *bundle, double simTime) {
     for (auto candidate : candidates) {
         if (nextHop = eidsByCandidate.at(candidate); nextHop != 0) {
             bundle->setNextHopEid(nextHop);
-
+            saveToCache(bundle->getDestinationEid(), nextHop, simTime);
             std::cout << "Routing bundle " << bundle->getBundleId() << " to " << nextHop << std::endl;
             return;
         }
@@ -81,4 +95,29 @@ void RoutingAntop::storeBundle(BundlePkt *bundle, int nextHop) {
     } else {
         std::cout << "Enqueued bundle " << bundle->getBundleId() << " to SDR for next hop " << nextHop << std::endl;
     }
+}
+
+void RoutingAntop::saveToCache(int destinationEid, int nextHop, double simTime){
+    CacheEntry entry = {
+        .nextHop = nextHop,
+        .ttl = simTime + TTL_SEC
+    };
+
+    nextHopCache[destinationEid] = entry;
+}
+
+int RoutingAntop::getFromCache(int destinationEid, double simTime){
+    auto it = nextHopCache.find(destinationEid);
+    if(it != nextHopCache.end()){ // if found
+        CacheEntry entry = it->second;
+        if(simTime <= entry.ttl){
+            return entry.nextHop;
+        } else {
+            // Entry expired
+            nextHopCache.erase(it);
+            return 0;
+        }
+    }
+
+    return 0;
 }
