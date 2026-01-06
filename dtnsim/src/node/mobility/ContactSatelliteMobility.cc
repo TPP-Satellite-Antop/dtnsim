b7942cff68670b70d9e894b35d6d9b9b5b2bd49d
Gastón Frenkel <gst.frenkel@gmail.com>
2026-01-05 22:31:36 -0300

    add access to mobility information

--- dtnsim/src/node/mobility/ContactSatelliteMobility.cc
+++ dtnsim/src/node/mobility/ContactSatelliteMobility.cc
@@ -28,14 +28,36 @@ void ContactSatelliteMobility::setTargetPosition()
     targetPosition.y = lastPosition.y;
     nextChange =  simTime() + updateInterval;
 
-    const auto latLng = LatLng {deg2rad(getLatitude()), deg2rad(getLongitude())};
+    auto latLng = LatLng {deg2rad(getLatitude()), deg2rad(getLongitude())};
 
-    H3Index cell = 0;
+	const auto idx = getParentModule()->getSubmodule("norad")->par("satIndex").intValue();
+	if (idx == 0) return;
+
+	H3Index cell = 0;
     if (latLngToCell(&latLng, 0, &cell) != E_SUCCESS) {
-        std::cout << "Error converting lat long to cell" << std::endl;
+        std::cout << "Error converting lat lng to cell" << std::endl;
         return;
     }
 
-    std::cout << "Cell: " << cell << std::endl;
+	std::array<H3Index, 7> neighbors{};
+	if (const H3Error err = gridDisk(cell, 1, neighbors.data()); err != E_SUCCESS) {
+		std::cout << "Error obtaining cell " << std::hex << cell << " neighbors" << std::endl;
+        return;
+	}
+
+	const auto nodes = getParentModule()->getParentModule()->par("nodesNumber").intValue();
+	for (int i = idx+1; i <= nodes; i++) {
+		auto mobility = dynamic_cast<ContactSatelliteMobility*>(getSimulation()->getSystemModule()->getSubmodule("node", i)->getSubmodule("mobility"));
+
+	    latLng = LatLng {deg2rad(mobility->getLatitude()), deg2rad(mobility->getLongitude())};
+    	if (latLngToCell(&latLng, 0, &cell) != E_SUCCESS) {
+        	std::cout << "Error converting lat lng to cell" << std::endl;
+        	continue;
+    	}
+
+		if (std::find(neighbors.begin(), neighbors.end(), cell) != neighbors.end()) {
+    		std::cout << "Cell " << std::hex << cell << " is in the array" << std::endl;
+		}
+	}
 }
 } // namespace inet
