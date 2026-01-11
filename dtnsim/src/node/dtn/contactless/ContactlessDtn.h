c87784700160198fc5241f892eb4f384aec31751
Gastón Frenkel <gst.frenkel@gmail.com>
2026-01-10 21:26:37 -0300

    remove unused pending bundle field from contactless dtn

--- dtnsim/src/node/dtn/contactless/ContactlessDtn.h
+++ dtnsim/src/node/dtn/contactless/ContactlessDtn.h
@@ -46,7 +46,6 @@ class ContactlessDtn : public Dtn {
     // BundlesMap
     bool saveBundleMap_;
     ofstream bundleMap_;
-    vector<BundlePkt*> pendingBundles_;
     // Maps a FWD message to a neighboring node's EID. There can be up to one FWD message in flight
     // per EID. Existence of a FWD signals a forwarding loop for the related EID is currently being
     // executed.
