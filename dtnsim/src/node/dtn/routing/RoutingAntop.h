9c267c159b019f8264bd094e93ad5e8a8322cee5
Gastón Frenkel <gst.frenkel@gmail.com>
2026-01-22 23:17:56 -0300

    fix RoutingAntop.h

--- dtnsim/src/node/dtn/routing/RoutingAntop.h
+++ dtnsim/src/node/dtn/routing/RoutingAntop.h
@@ -5,29 +5,28 @@
 #include "h3api.h"
 #include <src/node/dtn/routing/RoutingDeterministic.h>
 
-
-
 class RoutingAntop : public RoutingDeterministic {
-  public:
-    using GetPosition = std::function<LatLng(int)>;
-    using GetNextMobilityUpdate = std::function<double()>;
+    public:
+        using GetPosition = std::function<LatLng(int)>;
+        using GetNextMobilityUpdate = std::function<double()>;
 
-    RoutingAntop(
-      Antop* antop,
-      int eid,
-      int nodes, const GetPosition &getPosition,
-                 const GetNextMobilityUpdate &getNextMobilityUpdate);
-    virtual ~RoutingAntop();
-    virtual void routeAndQueueBundle(BundlePkt *bundle, double simTime);
-    int getEidFromH3Index(H3Index idx, H3Index dst, int dstEid) const;
-    H3Index getH3Index(int eid) const;
+        RoutingAntop(
+            Antop* antop,
+            int eid,
+            int nodes, const GetPosition &getPosition,
+            const GetNextMobilityUpdate &getNextMobilityUpdate
+        );
+        virtual ~RoutingAntop();
+        void routeAndQueueBundle(BundlePkt *bundle, double simTime) override;
+        [[nodiscard]] int getEidFromH3Index(H3Index idx, H3Index dst, int dstEid) const;
+        [[nodiscard]] H3Index getH3Index(int eid) const;
 
-  private:
-    int resolution_;
-    int nodes;
-    GetPosition getPosition;
-    GetNextMobilityUpdate getNextMobilityUpdate_;
-    RoutingTable *routingTable;
+    private:
+        int resolution_;
+        int nodes;
+        GetPosition getPosition;
+        GetNextMobilityUpdate getNextMobilityUpdate_;
+        RoutingTable *routingTable;
 };
 
 #endif /* SRC_NODE_DTN_ROUTINGANTOP_H_ */
