e697a53a1cb05dbea1ffd621f108cce12cc9b513
Gastón Frenkel <gst.frenkel@gmail.com>
2026-04-18 12:29:39 -0300

    update styling

--- dtnsim/utils/resultPlots/jsonPlots.py
+++ dtnsim/utils/resultPlots/jsonPlots.py
@@ -5,14 +5,23 @@ import glob
 import matplotlib.pyplot as plt
 import re
 
+plt.rcParams.update({
+    "font.size": 18,
+    "axes.titlesize": 16,
+    "axes.labelsize": 16,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    "legend.fontsize": 14
+})
+
 COLORS = {
     "ANTOP": "#66c2a5",
     "CGR": "#fc8d62",
     "CGR-ONE": "#8da0cb"
 }
 
-BASE = "dtnsim/experiment_results"
-OUT = "dtnsim/utils/resultPlots/plots"
+BASE = "../../experiment_results"
+OUT = "./plots"
 
 def pretty_metric_name(name: str) -> str:
     words = re.sub(r'([a-z])([A-Z])', r'\1 \2', name)
@@ -35,6 +44,14 @@ def satellites_from_name(name):
         return int(satellites)
     except Exception as e:
         raise ValueError(f"Cannot parse satellites from scenario name: {name}")
+
+def short_scenario_name(name):
+    try:
+        params = name.split("-")[1]
+        parts = params.split("x")
+        return "x".join(parts[1:])
+    except:
+        return name
 
 def load_result(path):
     with open(path, "r") as f:
@@ -74,6 +91,8 @@ def delivery_ratio_plot(antop, cgr, cgr_one, faults):
         set(cgr_one_map.keys())
     )
 
+    xscenarios = [short_scenario_name(n) for n in all_names]
+
     x = list(range(len(all_names)))
 
     def values(map_):
@@ -82,20 +101,25 @@ def delivery_ratio_plot(antop, cgr, cgr_one, faults):
     plt.figure()
     plt.title(f"Delivery ratio — {faults}% faults")
     plt.ylabel("Arrival (%)")
+    plt.xlabel("Scenarios")
     plt.ylim(90, 101.5)
+    ticks = plt.yticks()[0]
+    ticks = [t for t in ticks if 90 < t <= 100]
 
-    plt.plot(x, values(antop_map), marker="o", label="ANTop", color=COLORS["ANTOP"])
-    plt.plot(x, values(cgr_map), marker="s", label="CGR (per-neighbor routes)", color=COLORS["CGR"], fillstyle="none")
-    plt.plot(x, values(cgr_one_map), marker="^", label="CGR (one-best route)", color=COLORS["CGR-ONE"])
+    plt.yticks(ticks)
 
-    plt.xticks(x, all_names, rotation=45, ha="right")
+    plt.plot(x, values(antop_map), marker="o", label="ANTop", color=COLORS["ANTOP"], markersize=12)
+    plt.plot(x, values(cgr_map), marker="s", label="CGR (per-neighbor)", color=COLORS["CGR"], fillstyle="none", markersize=12)
+    plt.plot(x, values(cgr_one_map), marker="^", label="CGR (one-best)", color=COLORS["CGR-ONE"], markersize=12)
+
+    plt.xticks(x, xscenarios)
     plt.grid(axis="y", linestyle="--", alpha=0.6)
     plt.legend()
     plt.tight_layout()
 
     os.makedirs(OUT, exist_ok=True)
-    path = os.path.join(OUT, f"delivery_ratio_{faults}-faults.png")
-    plt.savefig(path, dpi=300)
+    path = os.path.join(OUT, f"delivery_ratio_{faults}-faults.pdf")
+    plt.savefig(path, bbox_inches="tight")
     plt.close()
 
     print(f"Saved {path}")
@@ -116,6 +140,8 @@ def triple_boxplot(metric, antop, cgr, cgr_one, ylabel, faults):
         set(cgr_map.keys()) |
         set(cgr_one_map.keys())
     )
+
+    xscenarios = [short_scenario_name(n) for n in all_names]
 
     data = []
     positions = []
@@ -148,16 +174,18 @@ def triple_boxplot(metric, antop, cgr, cgr_one, ylabel, faults):
         median.set_color("black")
         median.set_linewidth(2)
 
-    plt.xticks(range(len(all_names)), all_names, rotation=45, ha="right")
-    plt.suptitle(f"{pretty_metric_name(metric)} — {faults}% faults", y=0.95)
+    plt.xticks(range(len(all_names)), xscenarios)
+    plt.suptitle(f"{pretty_metric_name(metric)} — {faults}% faults", y=0.90)
     plt.ylabel(ylabel)
+    plt.xlabel("Scenarios")
+    plt.xlim(-0.5, len(all_names) - 0.5)
     plt.grid(axis="y", linestyle="--", alpha=0.6)
 
     plt.legend(
         handles=[
             plt.Line2D([0], [0], color=COLORS["ANTOP"], lw=6, label="ANTop"),
-            plt.Line2D([0], [0], color=COLORS["CGR-ONE"], lw=6, label="CGR (one-best route)"),
-            plt.Line2D([0], [0], color=COLORS["CGR"], lw=6, label="CGR (per-neighbor routes)"),
+            plt.Line2D([0], [0], color=COLORS["CGR-ONE"], lw=6, label="CGR (one-best)"),
+            plt.Line2D([0], [0], color=COLORS["CGR"], lw=6, label="CGR (per-neighbor)"),
         ],
         loc="upper center",
         bbox_to_anchor=(0.5, 1.15),
@@ -167,8 +195,8 @@ def triple_boxplot(metric, antop, cgr, cgr_one, ylabel, faults):
 
     plt.tight_layout(rect=[0, 0, 1, 0.985])
     os.makedirs(OUT, exist_ok=True)
-    path = os.path.join(OUT, f"boxplot_{metric}_{faults}-faults.png")
-    plt.savefig(path, dpi=300)
+    path = os.path.join(OUT, f"boxplot_{metric}_{faults}-faults.pdf")
+    plt.savefig(path, bbox_inches="tight")
     plt.close()
 
     print(f"Saved {path}")
