5a316c790b8a96a7e40d7d4514faac690ea65ccc
valenadels <vadelsflugel@fi.uba.ar>
2026-04-20 21:43:29 -0300

    path

--- dtnsim/utils/resultPlots/jsonPlots.py
+++ dtnsim/utils/resultPlots/jsonPlots.py
@@ -45,8 +45,8 @@ mpl.rcParams.update({
     "legend.fontsize": 14
 })
 
-BASE = "dtnsim/experiment_results"
-OUT = "dtnsim/utils/resultPlots/plots"
+BASE = "../../experiment_results"
+OUT = "./plots"
 
 def pretty_metric_name(name: str) -> str:
     words = re.sub(r'([a-z])([A-Z])', r'\1 \2', name)
